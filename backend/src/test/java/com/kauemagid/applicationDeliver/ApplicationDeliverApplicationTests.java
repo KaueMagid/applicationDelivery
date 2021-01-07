6c970ae3f8c9f9c36b5a3dd0735d65c4ee4a7d41
Kaue Salem <magidsalem96@gmail.com>
2021-01-07 12:44:23 -0300

    First Commit

---
+++ backend/src/test/java/com/kauemagid/applicationDeliver/ApplicationDeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauemagid.applicationDeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationDeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
